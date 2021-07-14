3bfc44a28744089075c09af308f2e783e628f19b
rogxo <1910500309@qq.com>
2021-07-15 01:23:30 +0800

    1.1.2

--- generate_pass.py
+++ generate_pass.py
@@ -1,13 +1,28 @@
 f = open('password.txt', mode='a+')
+
+gender = input("Gender(M/F):")
+
 a = input()
 flag = int(a[0])
 print(flag)
 b = input()
 
 if flag == 0:
-    for i in range(int(a), int(b)):
-        f.write('0' + str(i) + '\n')
-
+    if gender == 'M':
+        for i in range(int(a), int(b)):
+            if (i % 2) != 0:
+                f.write('0' + str(i) + '\n')
+    if gender == 'F':
+        for i in range(int(a), int(b)):
+            if (i % 2) == 0:
+                f.write('0' + str(i) + '\n')
 if flag:
-    for i in range(int(a), int(b)):
-        f.write(str(i) + '\n')
+    if gender == 'M':
+        for i in range(int(a), int(b)):
+            if (i % 2) != 0:
+                f.write(str(i) + '\n')
+    if gender == 'F':
+        for i in range(int(a), int(b)):
+            if (i % 2) == 0:
+                f.write(str(i) + '\n')
+f.close()
